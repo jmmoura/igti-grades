d724c367c6d5671746ad6f8851379d7d88903fd4
jmmoura <josielmartinsmoura@gmail.com>
2021-04-28 18:38:41 -0300

    origin URL modified

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-import { db } from './models/index.js';
+import { db } from './models/index.js'
+;(async () => {
+    try {
+        await db.mongoose.connect(db.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+    } catch (error) {
+        process.exit()
+    }
+})()
 
-(async () => {
-  try {
-    await db.mongoose.connect(db.url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (error) {
-    process.exit();
-  }
-})();
-
-const app = express();
+const app = express()
 
 //define o dominio de origem para consumo do servico
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
-);
+    cors({
+        origin: 'https://grades-app-test.herokuapp.com/',
+    })
+)
 
 app.get('/', (req, res) => {
-  res.send('API em execucao');
-});
+    res.send('API em execucao')
+})
 
-app.listen(process.env.PORT || 8081, () => {});
+app.listen(process.env.PORT || 8081, () => {})
