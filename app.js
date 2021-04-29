d68c81799dba710a6143b35bc12b5951600618f6
jmmoura <josielmartinsmoura@gmail.com>
2021-04-29 11:31:03 -0300

    app.js > cors origin modified

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
     cors({
-        origin: 'https://grades-app-test.herokuapp.com/',
+        origin: 'https://grades-app-test.herokuapp.com',
     })
 )
 app.use('/', gradeRouter)
